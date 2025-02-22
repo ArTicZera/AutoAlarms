0ce580be7946ad45f734d2cb5073e6e7192fd72a
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-02-22 18:48:35 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarmes_Automatico
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+
+        public Dictionary<string, int> alarmes = new Dictionary<string, int>
+        {
+            { "07:30", 27 },
+            { "08:20", 7  },
+            { "09:10", 7  },
+            { "10:00", 7  },
+            { "10:20", 27 },
+            { "11:10", 7  },
+            { "12:00", 7  },
+            { "13:00", 27 },
+            { "13:50", 7  },
+            { "14:40", 7  },
+            { "15:00", 27 },
+            { "15:50", 7  },
+            { "16:40", 7  }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer { Interval = 1000 };
+            timer.Tick += timer1_Tick;
+            timer.Start();
+
+            FillAlarms();
+        }
+        public void FillAlarms()
+        {
+            listBoxAlarmes.Items.Clear();
+            
+            foreach (var alarme in alarmes)
+            {
+                listBoxAlarmes.Items.Add($"Hora: {alarme.Key}, Duração: {alarme.Value - 2} segundos");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string horaAtual = DateTime.Now.ToString("HH:mm");
+
+            if (alarmes.ContainsKey(horaAtual))
+            {
+                int duracao = alarmes[horaAtual];
+                bool vinheta = (duracao == 27);
+
+                PlayAlarm(duracao, vinheta);
+            }
+        }
+
+        private void PlayAlarm(int seconds, bool vinheta)
+        {
+            using (SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.alarme))
+            {
+                soundPlayer.Play();
+
+                System.Threading.Thread.Sleep(seconds * 1000);
+
+                soundPlayer.Stop();
+            }
+
+            if (vinheta)
+            {
+                using (SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.vinheta))
+                {
+                    soundPlayer.Play();
+                    System.Threading.Thread.Sleep(29 * 1000);
+                    soundPlayer.Stop();
+                }
+            }
+
+            System.Threading.Thread.Sleep(60 * 1000);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Criado por Jhonatan Cordeiro Lopes (22/02/2025 - 2 Ano A)", "Créditos");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.vinheta);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            modHorario form = new modHorario(this, alarmes);
+            form.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ modHorario.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarmes_Automatico
+{
+    public partial class modHorario : Form
+    {
+        public Dictionary<string, int> alarmes;
+        private Form1 mainForm;
+
+        public modHorario(Form1 form, Dictionary<string, int> alarmes)
+        {
+            InitializeComponent();
+
+            FillAlarms(alarmes);
+
+            mainForm = form;
+            this.alarmes = alarmes;
+        }
+
+        private void FillAlarms(Dictionary<string, int> alarmes)
+        {
+            listBoxAlarmes.Items.Clear();
+
+            foreach (var alarme in alarmes)
+            {
+                listBoxAlarmes.Items.Add($"Hora: {alarme.Key}, Duração: {alarme.Value - 2} segundos");
+            }
+        }
+
+        public Dictionary<string, int> GetAlarmes()
+        {
+            return alarmes;
+        }
+
+        private void modHorario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Adicionar
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.", "Erro");
+                return;
+            }
+
+            string hora = textBox1.Text.Trim();
+
+            if (!int.TryParse(textBox2.Text.Trim(), out int duracao) || duracao < 0)
+            {
+                MessageBox.Show("Duração deve ser um número inteiro positivo.", "Erro");
+                return;
+            }
+
+            if (!alarmes.ContainsKey(hora))
+            {
+                alarmes.Add(hora, duracao);
+                FillAlarms(alarmes);
+                MessageBox.Show($"Alarme {hora} adicionado com duração de {duracao} segundos.", "Alarme Adicionado");
+            }
+            else
+            {
+                MessageBox.Show("Este horário já está cadastrado.", "Erro");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Remover
+
+            if (listBoxAlarmes.SelectedItem != null)
+            {
+                string itemSelecionado = listBoxAlarmes.SelectedItem.ToString();
+
+                string hora = itemSelecionado.Split(',')[0].Replace("Hora: ", "").Trim();
+
+                if (alarmes.ContainsKey(hora))
+                {
+                    alarmes.Remove(hora);
+                    FillAlarms(alarmes);
+                    MessageBox.Show($"Alarme {hora} removido com sucesso!", "Removido");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione um alarme para remover.", "Erro");
+            }
+
+            GetAlarmes();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Aplicar
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Cancelar
+            mainForm.alarmes = alarmes;
+            mainForm.FillAlarms();
+            this.Close();
+
+            //Atualiza o alarmes do Form1
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            //Horario
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            //Duracao
+        }
+    }
+}
